e5e106391f0bbb700383bd1609619a406eeefec1
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 18:56:14 +0300

    A template for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
       I18n.t('nav.about')                   => 'TODO',
     }
   end
+
+  def flash_to_alert_type(flash_type)
+    case flash_type.to_sym
+    when :notice then 'success'
+    when :alert  then 'error'
+    else              'info'
+    end
+  end
 end
