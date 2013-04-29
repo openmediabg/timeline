20449fce7dc254c82771ae339cc97d5ca1ddfd6d
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 22:17:08 +0300

    Don't load the app on production while precompiling assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ BulgarianMediaTimeline::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
