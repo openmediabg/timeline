4cd813103b6294e0e5cd1ef5438cca6494ddfde6
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 18:15:34 +0300

    Make the secret token really secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BulgarianMediaTimeline::Application.config.secret_token = 'fa58d28b425bbbba0ef1e9113cdf248d14de169efea19c09fb56c5c292cc59265502dbfbc361c4f6fc0a71acdd3a9748227ad34b92a6f57c0d9a913eb93fdd99'
+BulgarianMediaTimeline::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'fa58d28b425bbbba0ef1e9113cdf248d'
