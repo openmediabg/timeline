24b05d8653a4c5e10b4a4af7ab59f0459a063ca0
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 22:30:57 +0300

    Minor configuration tweak

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module BulgarianMediaTimeline
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = (ENV['DEFAULT_LOCALE'] || :bg).to_sym
-    config.i18n.available_locales = (ENV['AVAILABLE_LOCALES'] || 'bg').split(',')
+    config.i18n.available_locales = (ENV['AVAILABLE_LOCALES'] || 'bg').split(',').map(&:to_sym)
     config.i18n.locale = config.i18n.default_locale
 
     # Configure the default encoding used in templates for Ruby 1.9.
