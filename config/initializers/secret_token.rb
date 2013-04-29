dc53da6dd3488c2d3bb5352275619907c2a2fd7c
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 17:53:54 +0300

    Default Rails 3.2.13 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BulgarianMediaTimeline::Application.config.secret_token = 'fa58d28b425bbbba0ef1e9113cdf248d14de169efea19c09fb56c5c292cc59265502dbfbc361c4f6fc0a71acdd3a9748227ad34b92a6f57c0d9a913eb93fdd99'
