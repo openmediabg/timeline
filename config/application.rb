e5d8e5a3e50378fa93d6728b5f05fbc11bda103a
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 22:19:29 +0300

    Attempt to fix Heroku errors during deployment

--- config/application.rb
+++ config/application.rb
@@ -66,5 +66,6 @@ module BulgarianMediaTimeline
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
