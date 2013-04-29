7151a149ff0ec65d677bd785be6916c8adba2704
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 18:55:08 +0300

    Add a simple top navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def markup(text)
     Markup.format text
   end
+
+  def top_navigation_urls
+    {
+      I18n.t('nav.home')                    => root_path,
+      I18n.t('nav.about')                   => 'TODO',
+    }
+  end
 end
