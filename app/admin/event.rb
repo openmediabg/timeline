145de5b368ec47ca6bdebde8b19914c622ae3d1b
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 22:00:41 +0300

    Custom listing and edit/create form for events in the admin

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,2 +1,26 @@
 ActiveAdmin.register Event do
+  index do
+    selectable_column
+    id_column
+    column :category
+    column :title
+    column :start_date
+    column :end_date
+    column :published
+    column :priority
+    default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :category
+      f.input :title
+      f.input :start_date, start_year: 1980, end_year: Date.current.year
+      f.input :end_date, start_year: 1980, end_year: Date.current.year
+      f.input :description
+      f.input :published
+      f.input :priority
+    end
+    f.actions
+  end
 end
