f84abdfcc159db81df903dc98887f06a93a7bfab
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-29 21:53:18 +0300

    Minor ActiveAdmin configuration tweaks

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,16 +1,10 @@
 ActiveAdmin.setup do |config|
-
-  # == Site Title
-  #
-  # Set the title that is displayed on the main layout
-  # for each of the active admin pages.
-  #
-  config.site_title = "Bulgarian Media Timeline"
+  config.site_title = ->(_) { I18n.t('application.name') }
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = '/'
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
