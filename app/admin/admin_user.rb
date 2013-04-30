50d56ce94494326510115c3fff1cbe3eee650864
Dimitar Dimitrov <me@ddimitrov.name>
2013-04-30 11:21:23 +0300

    Custom show page for admin users in the admin

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register AdminUser do
   index do
+    selectable_column
+    id_column
     column :email
     column :current_sign_in_at
     column :last_sign_in_at
@@ -16,5 +18,25 @@ ActiveAdmin.register AdminUser do
       f.input :password_confirmation
     end
     f.actions
+  end
+
+  show do |resource|
+    panel I18n.t('texts.details') do
+      attributes_table_for resource do
+        row :id
+        row :email
+        row :sign_in_count
+        row :current_sign_in_at
+        row :last_sign_in_at
+        row :current_sign_in_ip
+        row :last_sign_in_ip
+        row :reset_password_token
+        row :reset_password_sent_at
+        row :remember_created_at
+        row :created_at
+        row :updated_at
+      end
+    end
+    active_admin_comments
   end
 end
