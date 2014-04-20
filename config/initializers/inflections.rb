2ac0d71719267bf4ae229bb8b7cff068014b95e6
Dimitar Dimitrov <me@ddimitrov.name>
2014-04-20 12:44:02 +0300

    Declare "media" as uncountable

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,6 +10,6 @@
 # end
 #
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.uncountable %w(media)
+end
