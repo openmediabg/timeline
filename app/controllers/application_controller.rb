b06773079d2817b76ef8d05b99b0bdce2727fe50
Dimitar Dimitrov <me@ddimitrov.name>
2014-04-26 20:01:16 +0300

    Add a permanent redirect to our new deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ require_dependency 'translated_flash_messages'
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :redirect_to_official_deployment
+
   private
+
+  def redirect_to_official_deployment
+    uri = URI.parse request.url
+    uri.hostname = 'timeline.openmedia.bg'
+    redirect_to uri.to_s, status: 301
+  end
 
   include TranslatedFlashMessages
 end
